af1de5c458eee1ba0607f0bcee2284f68b4052f3
awadieudone <awadieudone@gmail.com>
2019-08-02 21:32:38 +0100

    Updates

---
+++ precache-manifest.15863b25366f6f160fa6377265fbcabc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c26364be74bad64774bd028781cc7e",
+    "url": "/Form-Validation-ReactJS/index.html"
+  },
+  {
+    "revision": "92bbbb92ce041bf0867e",
+    "url": "/Form-Validation-ReactJS/static/css/main.14b243c4.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Form-Validation-ReactJS/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "92bbbb92ce041bf0867e",
+    "url": "/Form-Validation-ReactJS/static/js/main.8a233eff.chunk.js"
+  },
+  {
+    "revision": "6e733082df69e88b6e93",
+    "url": "/Form-Validation-ReactJS/static/js/runtime~main.6c36c6ac.js"
+  }
+]);
